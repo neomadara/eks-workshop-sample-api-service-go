3f65b1b7cb23e6950b29b8c24705c5464d7fddda
Cristian Gutiérrez <instanciageek@gmail.com>
2020-09-12 18:33:19 -0300

    no-history: change text to test trigger pipeline

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World, its me! Z3R0"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
